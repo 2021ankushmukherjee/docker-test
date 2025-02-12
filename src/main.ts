82e7964d7fb3f0d9c44d34ad5be31afbeb85c56f
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2025-02-13 03:13:59 +0530

    cdhdhj

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log("APP IS RUNNING ON PORT 3000");
+  console.log("APP IS RUNNING ON PORT 300");
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
